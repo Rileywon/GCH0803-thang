4fbea94d2dcce77a884c794cf882d75ef3288df4
Rileywon <thangntgch190892@fpt.edu.vn>
2021-08-24 11:02:10 +0700

    submit

--- day4/app.js
+++ day4/app.js
@@ -1,43 +1,45 @@
 const EXPRESS = require ('express')
-const {ObjectId,MongoClient, Int32 } = require('mongodb');
+const {Int32 } = require('mongodb');
 
-const { getDB } = require('./databasehandler');
+const { insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentByID } = require('./databasehandler');
 
 const APP = EXPRESS()
-const url = 'mongodb://localhost:27017';
 
 APP.use(EXPRESS.urlencoded({extended:true}))
 APP.set('view engine', 'hbs')
 
+APP.get('/edit',async (req,res)=>{
+    const idInput = req.query.id;
+    const search_Student = await getStudentByID(idInput);
+    res.render('edit',{student:search_Student})
+})
+
 APP.post('/insert',async (req,res)=>{
     const nameInput = req.body.txtName;
     const tuoiInput = req.body.txtTuoi;
-    const newStudent = {name:nameInput,tuoi: Int32(tuoiInput)};
-    const dbo = await getDB();
-    const newS = await dbo.collection("students").insertOne(newStudent);
-    console.log("gia tri moi cua insert la : ", newS.insertedId.toHexString());
+    const pictureInput = req.body.picture;
+    const newStudent = {name:nameInput,tuoi: Int32(tuoiInput),picture:pictureInput};
+    await insertStudent(newStudent);
     res.redirect('/');
 })
 
-APP.get('/delete', async(req,res)=>{
+APP.get('/delete',async(req,res)=>{
     const idInput = req.query.id;
-    const dbo = await getDB();
-    await dbo.collection("students").deleteOne({_id:ObjectId(idInput)});
+    await deleteStudent(idInput);
     res.redirect('/');
 })
 
 APP.get('/',async (req,res)=>{
-    const dbo = await getDB();
-    const allStudents = await dbo.collection("students").find({}).toArray();
-    res.render('index',{data:allStudents})
+    const allStudents = await getAllStudent();
+    res.render('index',{data:allStudents}); 
 })
 
 APP.post('/search',async (req,res)=>{
     const Search = req.body.txtSearch;
-    const dbo = await getDB();
-    const allStudents = await dbo.collection("students").find({name:Search}).toArray();
+    const allStudents = await searchStudent(Search);
     res.render('index',{data:allStudents})
 })
 
 const PORT = process.env.PORT || 5000;
 APP.listen(PORT);
+
--- day4/databasehandler.js
+++ day4/databasehandler.js
@@ -1,11 +1,39 @@
-const {MongoClient} = require('mongodb');
+const {MongoClient,ObjectId} = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+//const URL = 'mongodb://localhost:27017';
+const URL = "mongodb+srmongodb+srv://new-thang-99:27112011@cluster0.xogzz.mongodb.net/testv://new-thang-99:Thang1999+@cluster0.xogzz.mongodb.net/test";
+const DATABASE_NAME = "GCH0803DB"
 
 async function getDB() {
-    const client = await MongoClient.connect(url);
-    const dbo = client.db("GCH0803DB");
+    const client = await MongoClient.connect(URL);
+    const dbo = client.db(DATABASE_NAME);
     return dbo;
 }
 
-module.exports = {getDB}
+async function insertStudent(newStudent) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("students").insertOne(newStudent);
+    console.log("gia tri moi cua insert la : ", newS.insertedId.toHexString());
+}
+
+async function deleteStudent(idInput) {
+    const dbo = await getDB();
+    await dbo.collection("students").deleteOne({ _id: ObjectId(idInput) });
+}
+
+async function searchStudent(Search) {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({ name: Search }).toArray();
+    return allStudents;
+}
+
+async function getAllStudent() {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({}).toArray();
+    return allStudents;
+}
+async function getStudentByID(idInput){
+    const dbo = await getDB();
+    return dbo.collection("students").findOne({__id:ObjectId(idInput)});
+}
+    module.exports = {getDB,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentByID}
